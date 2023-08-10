95ae8a688b68a7d98abc879271b12e06ce339e22
rahimatgit <rahimmonjur9@gmail.com>
2023-08-09 23:03:41 -0700

    all done

--- fourth.js
+++ fourth.js
@@ -1,7 +1,23 @@
 function findAddress(obj) {
-   return Object.values(obj);
+   let keys = Object.keys(obj);
+   let values = Object.values(obj);
+   if(keys.length === 2){
+        values[2] = values[1];
+        values[1] = '__';
+        return values;
+   }
+   else if (keys.length === 1){
+        values[1] = '__';
+        values[2] = '__';
+        return values;
+   }
+   else{
+    return values;
+   }
 }
 
-let sample = {street: 10, house: '15A', society: 'Earth Perfect'};
+// let sample = {street: 10, house: '15A', society: 'Earth Perfect'};
+let sample = {street: 10,  society: 'Earth Perfect'};
+
 console.log(findAddress(sample));
     
